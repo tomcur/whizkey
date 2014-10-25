c752a9f162983370e2e5622fc4f43215145e6146
Thomas Churchman <thomas.c@desktop>
2014-10-25 23:41:33 +0200

    Initial commit

---
+++ crypto/password.js
@@ -0,0 +1,211 @@
+/**
+* Class that provides functionality to convert data into a password.
+* @params {string} firstName First name data.
+* @params {string} birthPlace Birthplace data.
+* @params {string} domain Domain data.
+* @params {string} accountName Account name data.
+* @params {string} counter Counter data.
+* @params {string} masterPassword Master password.
+*/
+function DataToPassword(firstName, birthPlace, domain, accountName, counter, masterPassword)
+{
+	var byteToChar = new ByteToChar();
+	
+	// Concatenate the data
+	var msg = firstName.toLowerCase()
+		+ birthPlace.toLowerCase()
+		+ domain.toLowerCase()
+		+ accountName.toLowerCase()
+		+ counter
+		+ masterPassword;
+	
+	// Digest and convert the data to an array of byte values
+	var md = forge.md.sha512.create();
+	md.update(msg);
+	var hex = md.digest().toHex();
+	var bytes = forge.util.hexToBytes(hex);
+	b = [];
+	for (var i = 0; i < bytes.length; i++ ) 
+	{
+		b.push(bytes.charCodeAt(i));
+	}	
+	
+	/**
+	* Generates an array of long password styles and uses that
+	* to call this.Password(-) to generate a password.
+	* @returns {string} The generated password.
+	*/
+	this.GetLongPassword = function()
+	{
+		start = ['Cvcv', 'Cvvc', 'CvCv'];
+		middle = ['Cvcv', 'Cvvc'];
+		addOn = ['#n', '#n'];
+		
+		total = [];
+		
+		for(var s in start)
+		{
+			for(var m1 in middle)
+			{
+				for(var m2 in middle)
+				{
+					for(var m3 in middle)
+					{
+						for(var a1 in addOn)
+						{
+							for(var a2 in addOn)
+							{
+								total.push(start[s] + addOn[a1] + middle[m1] + addOn[a2] + middle[m2] + middle[m3]);
+								total.push(start[s] + addOn[a1] + middle[m1] + middle[m2] + addOn[a2] + middle[m3]);
+								total.push(start[s] + addOn[a1] + middle[m1] + middle[m2] + middle[m3] + addOn[m3]);
+								
+								total.push(start[s] + middle[m1] + addOn[a1] + middle[m2] + addOn[a2] + middle[m3]);
+								total.push(start[s] + middle[m1] + addOn[a1] + middle[m2] + middle[m3] + addOn[a2]);
+								
+								total.push(start[s] + middle[m1] + middle[m2] + addOn[a1] + middle[m3] + addOn[a2]);
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return this.Password(total);
+	};
+		
+	/**
+	* Generates an array of medium password styles and uses that
+	* to call this.Password(-) to generate a password.
+	* @returns {string} The generated password.
+	*/
+	this.GetMediumPassword = function()
+	{
+		start = ['Cvcv', 'Cvvc', 'CvCv'];
+		middle = ['Cvcv', 'Cvvc', 'CvCv'];
+		addOn = ['n#', '#n'];
+		
+		total = [];
+		
+		for(var s in start)
+		{
+			for(var m1 in middle)
+			{
+				for(var m2 in middle)
+				{
+					for(var a1 in addOn)
+					{
+						total.push(start[s] + addOn[a1] + middle[m1] + middle[m2]);
+						total.push(start[s] + addOn[a1] + middle[m1] + middle[m2]);
+						total.push(start[s] + addOn[a1] + middle[m1] + middle[m2]);
+						
+						total.push(start[s] + middle[m1] + addOn[a1] + middle[m2]);
+						total.push(start[s] + middle[m1] + addOn[a1] + middle[m2]);
+						
+						total.push(start[s] + middle[m1] + middle[m2] + addOn[a1]);
+					}
+				}
+			}
+		}
+		
+		return this.Password(total);
+	};
+	
+	/**
+	* Creates a password from one of the password styles given.
+	* @param {array} passwords The password styles to choose from.
+	* @returns {string} The generated password.
+	*/
+	this.Password = function(passwords)
+	{
+		p = b[0] % passwords.length;
+		
+		password = "";
+		
+		i = 1;
+		for(var key in passwords[p])
+		{
+			var cType = passwords[p][key];
+			password += byteToChar.GetChar(cType, b[i]);
+			//console.log(cType);
+			//console.log(b[i]);
+			//console.log("-----");
+			i++;
+		}
+		
+		return password;
+	}
+}
+
+/**
+* Class to provide byte to character conversion.
+*/
+function ByteToChar()
+{
+	var c = 
+		[
+			'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
+			'l', 'm', 'n', 'p', 'q', 'r', 's', 't',
+			'v', 'w', 'x', 'y', 'z'
+		];
+
+	var C = [];
+
+	var v =
+		[
+			'a', 'e', 'i', 'o', 'u', 'y'
+		];
+
+	var V = [];
+
+	var symbols =
+		[
+			'!', '@', '#', '%', '&', '*', '(', ')',
+			'-', '+', '_', '?'
+		];
+
+	var n =
+		[
+			'0', '1', '2', '3', '4', '5', '6', '7',
+			'8', '9'
+		];
+
+	var x = [];
+		
+	// Fill other arrays
+	for(var key in c)
+	{
+		C.push(c[key].toUpperCase());
+	}
+	for(var key in v)
+	{
+		V.push(v[key].toUpperCase());
+	}
+	x = c.concat(C.concat(v.concat(V.concat(symbols.concat(n.concat(x))))));
+	
+	/**
+	* Convert one byte (as integer) to a character.
+	* @param {char} type The type of character to convert the byte to.
+	* @param {number} b The byte (as integer) to convert.
+	* @returns {char} The converted character.
+	*/
+	this.GetChar = function(type, b)
+	{
+		switch(type)
+		{
+			case 'c':
+				return c[b % c.length];
+			case 'C':
+				return C[b % C.length];
+			case 'v':
+				return v[b % v.length];
+			case 'V':
+				return V[b % V.length];
+			case 'n':
+				return n[b % n.length];
+			case '#':
+				return symbols[b % symbols.length];
+			case 'x':
+				return x[b % x.length];	
+		}
+	};
+}
---
+++ dialog/dialog_popup.js
@@ -0,0 +1,333 @@
+/**
+* Send a message to the background thread.
+* @param {object} msg The message to send.
+* @param {function} callback The callback to call.
+*/
+function MessageBackground(msg, callback)
+{
+	chrome.runtime.sendMessage(msg, function(response) 
+	{
+		callback(response);
+	});
+}
+
+/**
+* Send the new vault to the background thread for saving.
+* @param {vault} vault The vault to save.
+*/
+function SaveVault(vault)
+{
+	MessageBackground({greeting: "saveVault", data: vault.vault}, function(response)
+	{
+		// TO-DO: error checking
+	});
+}
+
+//var hashtest = CryptoJS.SHA512("test");
+//console.log("hash: " + hashtest);
+
+
+/**
+* Populate dialog information
+*/
+function Populate()
+{	
+	if(!vault.IsVault())
+	{
+		$('#content').html('<h2>Please select a vault first.</h2><p>Go to Options -> Add Vault.</p>');
+		return;
+	}
+	
+	MessageBackground({greeting: "getDomainInfo"}, function(response)
+	{
+		console.log(vault);
+		var host = $.url(response.url).attr('host');
+		tld = window.publicSuffixList.getDomain(host).toLowerCase();
+		$('.domainname').html(tld);
+		$('#default-vault').html(vault.GetName());
+		
+		var linkedTo = vault.GetDomainLink(tld);
+			
+		accounts = [];
+		var accountsDomain = vault.GetAccounts(tld);
+		var accountsLink = [];
+		if(linkedTo != null)
+		{
+			var accountsLink = vault.GetAccounts(linkedTo);
+			$('#domain-link-title').html('(' + linkedTo + ')');
+		}
+		
+		var i = 0;
+		// Merge domain and link accounts
+		for(var key in accountsDomain)
+		{
+			accounts.push({"id": i, "account": accountsDomain[key], "val": accountsDomain[key], "domain": tld});
+			i++;
+		}
+		for(var key in accountsLink)
+		{
+			accounts.push({"id": i, "account": accountsLink[key], "val": accountsLink[key] + " (link)", "domain": linkedTo});
+			i++;
+		}
+		
+		//var accounts = vault.GetAccounts(tld);
+
+		
+		console.log(accounts);
+		// Add accounts to account selection
+		for(var key in accounts)
+		{
+			var account = accounts[key];
+			$('#account').append($('<option/>', { 
+				value: account["id"],
+				text : account["val"]
+			}));
+		}
+		
+		var domains = vault.GetDomainNames();
+		
+		// Add domains to domain selection
+		for(var key in domains)
+		{
+			var domain = domains[key];
+			var linkedTo = vault.GetDomainLink(domain);
+			if(domain == tld || linkedTo != null)
+			{
+				continue;
+			}
+			
+			$('#linkto').append($('<option/>', { 
+				value: domain,
+				text : domain
+			}));
+		}
+	});
+}
+
+/**
+* Add an account
+*/
+function AddAccount()
+{
+	var name = $("#newaccount-name").val();
+	var checked = $("#newaccount-advanced").prop('checked');
+	
+	var count = 1;
+	var type = "medium";
+	if(checked)
+	{
+		count = parseInt($("#newaccount-count").val());
+		type = parseInt($("#newacount-type").val());
+	}
+	
+	if(name == "")
+	{
+		$("#newaccount-status").html("<p>Please enter an account name.</p>");
+	}
+	else if(!(count >= 1))
+	{
+		$("#newaccount-status").html("<p>Please enter a count of 1 or higher.</p>");
+	}
+	else if(["secure", "long", "medium", "short", "pin"].indexOf(type) == -1)
+	{
+		$("#newaccount-status").html("<p>Please enter a valid password type.</p>");
+	}
+	else
+	{
+		var linkedTo = vault.GetDomainLink(tld);
+		var addTo = tld;
+		if(linkedTo != null)
+		{
+			addTo = linkedTo;
+		}
+		
+		if(vault.GetAccounts(addTo).indexOf(name) >= 0)
+		{
+			$("#newaccount-status").html("<p>That account already exists for " + addTo + "</p>");
+		}
+		else
+		{		
+			vault.AddAccount({"name": name, "type": type, "count": count}, addTo);
+			SaveVault(vault);
+			
+			
+			$("#whizkey-new-account").hide(100, function()
+			{
+				$("#whizkey-new-success").show(100);
+			});
+		}
+	}
+}
+
+/**
+* Generates a password for the given account and shows it.
+*/
+function Generate()
+{
+	var accountID = $('#account').val();
+	var account = accounts[accountID];
+	var masterPassword = $('#masterPassword').val();
+	
+	password = GeneratePassword(vault, account["domain"], account["account"], masterPassword);
+	
+	var mask = "";
+	for(i = 0; i < password.length; i++)
+	{
+		mask = mask + "*";
+	}
+	$('#password-mask-text').html(mask);
+	$('#password-nomask-text').html(password);
+	
+	$('#generate-button').hide();
+	$('#generated').show();
+}
+
+/**
+* Add the domain link.
+*/
+function LinkDomain()
+{
+	var linkTo = $("#linkto").val();
+	
+	// Unlink
+	if(linkTo == vault.GetDomainLink(tld))
+	{
+		if(vault.UnlinkDomain(tld))
+		{
+			SaveVault(vault);
+		
+			$("#linkto-status").html("Unlinked!");
+		}
+		else
+		{
+			$("#linkto-status").html("Could not unlink...");
+		}
+	}
+	else
+	{
+		if(vault.SetDomainLink(tld, linkTo))
+		{
+			SaveVault(vault);
+		
+			$("#linkto-status").html("Link was saved!");
+		}
+		else
+		{
+			$("#linkto-status").html("Link was not saved...");
+		}
+	}
+}
+
+/**
+* Copy text to clipboard.
+* @param {string} text The text to copy.
+*/
+function CopyToClipboard(text)
+{
+    var copyFrom = $('<textarea/>');
+    copyFrom.text(text);
+    $('body').append(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy', true);
+    copyFrom.remove();
+}
+
+$(function()
+{
+	password = "";
+
+	PageI18N();
+	
+	// Hide password by default, add toggle button
+	$('#password').hideShowPassword(false, true);
+	
+	// Return
+	$('.whizkey-return').click(function()
+	{
+		Populate();
+		$('#whizkey-new-account, #whizkey-link-domain, #whizkey-new-success').hide(100, function()
+		{
+			$('#whizkey-gen').show(100);
+		});
+	});
+	
+	$("#linkwebsite").click(function()
+	{
+		$("#whizkey-gen").hide(100, function()
+		{
+			$("#whizkey-link-domain").show(100);
+		});
+	});
+	
+	$("#close-button").click(function()
+	{
+		MessageBackground({greeting: "closeIFrame"});
+	});
+	
+	
+	// Generate, show, and copy buttons
+	$("#generate-button").click(function()
+	{
+		Generate();
+	});
+	
+	$("#password-copy").click(function()
+	{
+		CopyToClipboard(password);
+	});
+	
+	$("#password-generated").click(function()
+	{
+		$("#generated").hide();
+		$("#generated-nomask").show();
+	});
+	
+	$("#newaccount").click(function()
+	{
+		$("#whizkey-gen").hide(100, function()
+		{
+			$("#whizkey-new-account").show(100);
+		});
+	});
+	
+	$("#newaccount-advanced").on('change', function()
+	{
+		if(this.checked)
+		{
+			$("#whizkey-new-advanced").show(100);
+		}
+		else
+		{
+			$("#whizkey-new-advanced").hide(100);
+		}
+	});
+	
+	$("#newaccount-submit").click(function()
+	{
+		AddAccount();
+	});
+	
+	$("#linkto-submit").click(function()
+	{
+		LinkDomain();
+	});
+	
+	// Load TLD list
+	$.ajax({
+		type: 'GET',
+		url: '../data/effective_tld_names.dat',
+		success: function (list) 
+		{
+			window.publicSuffixList.parse(list, punycode.toASCII);
+			
+			// Load default vault
+			MessageBackground({greeting: "getDefaultVault"}, function(response) 
+			{
+				vault = new Vault(response.data);
+				// Populate dialog information
+				Populate();
+			});
+		}
+	});
+});
+
